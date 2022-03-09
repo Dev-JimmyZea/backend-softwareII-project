df7ab62ecf247422e0c54c5776ecb25eaa57ab6c
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-08 20:53:46 -0500

    feat(Root): Add methods deleteAll, addApplicant and removeApplicant for the work object

--- controllers/works.js
+++ controllers/works.js
@@ -114,6 +114,83 @@ module.exports = {
                 error: err
             });
         }
+    },
+
+    deleteAllWorks: async (req, res) => {
+        try {
+            const works = await Works.deleteMany({});
+            if (!works) {
+                return res.status(404).json({
+                    message: 'Works not found',
+                });
+            }
+            return res.status(200).json({
+                message: 'Works deleted successfully',
+                data: works
+            });
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Failed to delete works',
+                error: err
+            });
+        }
+    },
+
+    addApplicant: async (req, res) => {
+        try {
+
+            const work = await Works.findOneAndUpdate({
+                code: req.params.code
+            }, {
+                $push: {
+                    applicants: req.params.id
+                }
+            }, {
+                new: true
+            });
+            if (!work) {
+                return res.status(404).json({
+                    message: 'Work not found',
+                });
+            }
+            return res.status(200).json({
+                message: 'Applicant added successfully',
+                data: work
+            });
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Failed to add applicant',
+                error: err
+            });
+        }
+    },
+
+    removeApplicant: async (req, res) => {
+        try {
+            const work = await Works.findOneAndUpdate({
+                code: req.params.code
+            }, {
+                $pull: {
+                    applicants: req.params.id
+                }
+            }, {
+                new: true
+            });
+            if (!work) {
+                return res.status(404).json({
+                    message: 'Work not found',
+                });
+            }
+            return res.status(200).json({
+                message: 'Applicant removed successfully',
+                data: work
+            });
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Failed to remove applicant',
+                error: err
+            });
+        }
     }
 
 };
--- routes/works.js
+++ routes/works.js
@@ -2,13 +2,16 @@
 
 import { Router } from 'express';
 const router = Router();
-import { getWorks, getWork, createWork, updateWork, deleteWork } from '../controllers/works';
+import { getWorks, getWork, createWork, updateWork, deleteWork, deleteAllWorks, addApplicant, removeApplicant } from '../controllers/works';
 import { verifyToken } from '../utils/auth';
 
 router.get('/', getWorks);
 router.get('/:code', getWork);
-router.post('/', createWork);
+router.post('/', verifyToken, createWork);
 router.put('/:id', verifyToken, updateWork);
 router.delete('/:code',verifyToken, deleteWork);
+router.delete('/', verifyToken, deleteAllWorks);
+router.put('/:code/:id', verifyToken, addApplicant);
+router.put('/remove/:code/:id', verifyToken, removeApplicant);
 
 export default router;
