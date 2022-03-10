04e28a104dcf8205882d34a0a529b8017ef9346f
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-09 21:27:25 -0500

    feat(Root): Add file to variables config of app, db and cloudinary

---
+++ src/config.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV === 'development' ? require('dotenv').config() : null
+
+const config = {
+    appConfig: {
+        host: process.env.APP_HOST,
+        port: process.env.APP_PORT
+    },
+    dbConfig: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        name: process.env.DB_NAME
+    },
+    cloudinaryConfig: {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    }
+}
+
+export default config
