e00d5399e5df4dcaab63cb8df5d60cff0825e644
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-21 19:03:07 -0500

    fix(Root): Fix errors in storage path to save images locally

--- libs/storage.js
+++ libs/storage.js
@@ -2,9 +2,8 @@ import multer, { diskStorage } from 'multer'
 import path from 'path'
 
 const storage = diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, './public/imgs')
-    },
+    destination: path.join(__dirname, '../public/imgs'),
+
     filename: (req, file, cb) => {
         const url = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
         cb(null, url)
